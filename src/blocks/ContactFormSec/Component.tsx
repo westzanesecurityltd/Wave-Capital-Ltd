0738d406c467acce928fe8c58fc33ea84e51e234
Dhruv Nakum <dhruv.nakum@codezeros.com>
2025-11-12 11:44:04 +0530

    Added contact us form changes

--- src/blocks/ContactFormSec/Component.tsx
+++ src/blocks/ContactFormSec/Component.tsx
@@ -87,7 +87,7 @@ function ContactFormSec(props?: ContactFormSecProps) {
 
   return (
     <>
-      <section className="contactform-section relative min-h-[300px] lg:min-h-[500px] grid items-center py-12 lg:py-20 bg-[#E7F4FF]">
+      <section id='contact-form' className="contactform-section relative min-h-[300px] lg:min-h-[500px] grid items-center py-12 lg:py-20 bg-[#E7F4FF]">
         <div className="container relative z-10">
           <div className="grid grid-cols-12 gap-5 lg:gap-[40px] xl:gap-[60px]">
             <div className="col-span-12 lg:col-span-5 max-w-xl order-2 lg:order-1">
